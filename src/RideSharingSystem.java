62507d853a388141e0b83c30e00d262557902134
Ayush Sharma <asayush35@gmail.com>
2025-04-29 16:14:41 +0530

    Simplifying logic

--- src/RideSharingSystem.java
+++ src/RideSharingSystem.java
@@ -1,7 +1,10 @@
 package src;
 
+import com.google.common.util.concurrent.AtomicDouble;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class RideSharingSystem {
     private final Map<String, Customer> customers;
@@ -17,13 +20,18 @@ public class RideSharingSystem {
 
     public synchronized static RideSharingSystem getInstance() {
         if (instance == null) {
-            instance = new RideSharingSystem();
+            synchronized (RideSharingSystem.class) {
+                if (instance == null) {
+                    instance = new RideSharingSystem();
+                }
+            }
         }
         return instance;
     }
-    public Driver addDriver(String name, String phoneNumber, Vehicle vehicle) {
+
+    public Driver addDriver(String name, String phoneNumber, Vehicle vehicle, String license) {
         String driverId = generateDriverId();
-        Driver driver = new Driver(name, phoneNumber, driverId, vehicle);
+        Driver driver = new Driver(name, phoneNumber, driverId, vehicle, license);
         drivers.put(driverId, driver);
         System.out.println("Driver added with ID: " + driverId + ", Name: " + name);
         return driver;
@@ -40,11 +48,11 @@ public class RideSharingSystem {
 
     public void displayVehiclesAvailable() {
         System.out.println("Vehicles available:");
-        for (Driver driver : drivers.values()) {
+        drivers.values().forEach(driver -> {
             if (driver.isAvailable()) {
                 System.out.println("Driver: " + driver.getName() + ", Vehicle: " + driver.getVehicle().getModel() + ", " + driver.getVehicle().getVehicleType() + ", " + driver.getVehicle().getRegistrationNumber());
             }
-        }
+        });
     }
 
     public Customer addCustomer(String name, String phoneNumber) {
@@ -65,119 +73,108 @@ public class RideSharingSystem {
     }
 
     private String generateDriverId() {
-        return "D" + drivers.size();
+        return "D" + UUID.randomUUID().toString().replace("-", "");
     }
 
     private String generateCustomerId() {
-        return "C" + customers.size();
+        return "C" + UUID.randomUUID().toString().replace("-", "");
     }
 
-    public Ride createRide(String customerId, Location from, Location to) {
-        Customer customer = customers.get(customerId);
+    public Ride createRide(Customer customer, Location from, Location to) {
         if (customer == null) {
             System.out.println("Customer not found.");
             return null;
         }
+        System.out.println("Ride requested by: " + customer.getName());
         double fare = calculateFare(from, to);
         String rideId = generateRideId();
-        Ride ride = new Ride(rideId, from, to, customer, fare);
+        Driver driver = assignDriver(to);
+        if(driver == null) return null; // no driver available
+        Ride ride = new Ride(rideId, from, to, customer, fare, driver);
         rides.put(rideId, ride);
-        System.out.println("Ride created with ID: " + rideId);
-        if(assignDriver(rideId)) return ride; // assigning a driver for now
-        return null;
+        System.out.println("Ride created with ID: " + rideId + ", Fare: " + fare + ", Driver: " + driver.getName());
+        return ride;
     }
 
     private double calculateFare(Location startLocation, Location endLocation) {
-        double distance = Math.abs(startLocation.getLatitude() - endLocation.getLatitude()) + Math.abs(startLocation.getLongitude() - endLocation.getLongitude());
+        double distance = Math.abs(startLocation.latitude() - endLocation.latitude()) + Math.abs(startLocation.longitude() - endLocation.longitude());
         return distance * 10; // $10 per mile
     }
 
     private String generateRideId() {
-        return "R" + rides.size();
+        return "R" + UUID.randomUUID().toString().replace("-", "");
     }
 
-    public void cancelRide(Customer customer) {
+    public void cancelRideByCustomer(Customer customer) {
         // cancel the ride
         String customerId = customer.getId();
-        for (Map.Entry<String, Ride> entry : rides.entrySet()) {
-            Ride ride = entry.getValue();
-            if (ride.getCustomer().getId().equals(customerId) && ride.getStatus() != RideStatus.REQUESTED && ride.getStatus() != RideStatus.COMPLETED) {
-                ride.updateRideStatus(RideStatus.CANCELLED);
-                if (ride.getDriver() == null) {
+        rides.values().stream()
+                .filter(ride -> ride.getCustomer().getId().equals(customerId) && ride.getStatus() == RideStatus.REQUESTED)
+                .forEach(ride -> {
+                    ride.updateRideStatus(RideStatus.CANCELLED);
+                    ride.getDriver().setAvailability(true);
                     System.out.println("Ride cancelled by: " + customer.getName());
-                    return;
-                }
-                String driverId = ride.getDriver().getId();
-                Driver driver = drivers.get(driverId);
-                driver.setAvailability(true);
-                System.out.println("Ride cancelled by: " + customer.getName());
-                return;
-            }
-        }
-        System.out.println("Ride not found.");
+                });
     }
 
-    public void startRide(String driverId) {
-        Driver driver = drivers.get(driverId);
+    public void startRide(Driver driver) {
         if (driver == null) {
             System.out.println("Driver not found.");
             return;
         }
-        for(Ride ride : rides.values()) {
-            if (ride.getDriver().getId().equals(driverId) && ride.getStatus() == RideStatus.SCHEDULED) {
-                ride.updateRideStatus(RideStatus.ONGOING);
-                System.out.println("Ride started. Driver: " + driver.getName());
-                return;
-            }
-        }
+        rides.values().stream()
+                .filter(ride -> ride.getDriver().getId().equals(driver.getId()) && ride.getStatus() == RideStatus.REQUESTED)
+                .forEach(ride -> {
+                    ride.updateRideStatus(RideStatus.ONGOING);
+                    driver.updateLocation(ride.getCustomer().getLocation());
+                    System.out.println("Ride started by: " + driver.getName());
+                });
     }
 
-    public double completeRide(String driverId) {
-        Driver driver = drivers.get(driverId);
+    public double completeRide(Driver driver) {
         if (driver == null) {
             System.out.println("Driver not found.");
             return 0.0;
         }
-        for(Ride ride : rides.values()) {
-            if (ride.getDriver().getId().equals(driverId) && ride.getStatus() == RideStatus.ONGOING) {
-                ride.updateRideStatus(RideStatus.COMPLETED);
-                driver.setAvailability(true);
-                System.out.println("Ride completed. Driver: " + driver.getName());
-                System.out.println("Fare: " + ride.getFare());
-                return ride.getFare();
-            }
-        }
-        return 0.0;
+        AtomicDouble fare = new AtomicDouble(0.0);
+        rides.values().stream()
+                .filter(ride -> ride.getDriver().getId().equals(driver.getId()) && ride.getStatus() == RideStatus.ONGOING)
+                .forEach(ride -> {
+                    ride.updateRideStatus(RideStatus.COMPLETED);
+                    driver.setAvailability(true);
+                    driver.updateLocation(ride.getEndLocation());
+                    ride.getCustomer().updateLocation(ride.getEndLocation());
+                    fare.set(ride.getFare());
+                    System.out.println("Ride completed with fare: " + ride.getFare());
+                });
+        return fare.get();
     }
 
-    private boolean assignDriver(String rideId) {
-        for (Driver driver : drivers.values()) {
-            if (driver.isAvailable()) {
-                Ride ride = rides.get(rideId);
-                ride.assignDriver(driver);
-                driver.setAvailability(false);
-                ride.updateRideStatus(RideStatus.SCHEDULED);
-                return true;
-            }
-        }
-        System.out.println("No driver available for this ride.");
-        rides.get(rideId).updateRideStatus(RideStatus.CANCELLED);
-        return false;
+    private Driver assignDriver(Location location) {
+        return drivers.values().stream()
+                .filter(Driver::isAvailable).min((d1, d2) -> {
+                    long distance1 = getDistance(d1.getLocation(), location);
+                    long distance2 = getDistance(d2.getLocation(), location);
+                    return Long.compare(distance1, distance2);
+                })
+                .orElse(null);
     }
 
-    public void cancelRideByDriver(String driverId) {
-        Driver driver = drivers.get(driverId);
-        if (driver == null) {
-            System.out.println("Driver not found.");
-            return;
-        }
-        for(Ride ride : rides.values()) {
-            if (ride.getDriver().getId().equals(driverId) && ride.getStatus() == RideStatus.SCHEDULED) {
-                ride.updateRideStatus(RideStatus.CANCELLED);
-                driver.setAvailability(true);
-                System.out.println("Ride cancelled by driver: " + driver.getName());
-                return;
-            }
-        }
+    public void cancelRideByDriver(Driver driver) {
+        rides.values().stream()
+                .filter(ride -> ride.getDriver().getId().equals(driver.getId()) && ride.getStatus() == RideStatus.REQUESTED)
+                .forEach(ride -> {
+                    ride.updateRideStatus(RideStatus.CANCELLED);
+                    driver.setAvailability(true);
+                    System.out.println("Ride cancelled by driver: " + driver.getName());
+                });
+    }
+
+    public void makePayment(double amount, Customer customer, Payment paymentMethod) {
+        paymentMethod.makePayment(amount);
+        System.out.println("Payment of $" + amount + " made by customer: " + customer.getName());
+    }
+    private long getDistance(Location start, Location end) {
+        return (long) Math.sqrt(Math.pow(start.latitude() - end.latitude(), 2) + Math.pow(start.longitude() - end.longitude(), 2));
     }
 }
